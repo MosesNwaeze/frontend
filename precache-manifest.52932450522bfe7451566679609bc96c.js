9181fd970163a8f903f23a4590436e3feb030fb0
Moses Nwaeze <ogbonnasmart@gmail.com>
2019-12-22 03:53:33 +0100

    Updates

---
+++ precache-manifest.52932450522bfe7451566679609bc96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9298f1ce3446e9afb4d4393a8924721c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "685f3690bf400af0d13a",
+    "url": "/static/css/2.5c171af3.chunk.css"
+  },
+  {
+    "revision": "0fe78029f287b905cca0",
+    "url": "/static/css/main.5e4eff74.chunk.css"
+  },
+  {
+    "revision": "685f3690bf400af0d13a",
+    "url": "/static/js/2.3518104c.chunk.js"
+  },
+  {
+    "revision": "0fe78029f287b905cca0",
+    "url": "/static/js/main.fb56483d.chunk.js"
+  },
+  {
+    "revision": "2b42684769868e6e20fd",
+    "url": "/static/js/runtime-main.835aaafc.js"
+  }
+]);
